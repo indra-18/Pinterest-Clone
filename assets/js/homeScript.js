5b6c9805d2909957ebcd0892b737e75db86ae995
indra-18 <indraind0918@gmail.com>
2023-06-28 11:44:42 +0530

    fetch and display images from unsplash api

--- assets/js/homeScript.js
+++ assets/js/homeScript.js
@@ -6,3 +6,37 @@
 //       x.className = "topnav";
 //     }
 //   }
+const imgGallery = document.querySelector('.img-gallery');
+const navBar = document.getElementById('navbar');
+let images = ``;
+
+async function fetchRandomImages () {
+    try {
+        await fetch(`https://api.unsplash.com/photos/?client_id=2WhxTU-EOTsgEb3212OI-oATAc4Zsb81PaTdAzJSt_E`)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(image => {
+                images += `
+                <img class="image-card" src=${image.urls.small} alt=${image.title} />
+                `;
+                imgGallery.innerHTML = images
+            });
+        })
+    } catch (error) {
+        alert(error.message)
+    }
+}
+fetchRandomImages()
+
+window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > navBar.offsetTop) {
+            navBar.style.position = 'fixed';
+            navBar.style.top = '0';
+            navBar.style.scrollBehavior = 'smooth'
+        } else {
+            navBar.style.position = 'static';
+            navBar.style.scrollBehavior = 'smooth'
+        }
+    });
+})
